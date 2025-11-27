df9839d92b200c963e08c0bf98603c7f0c663351
daistic <matotsuper@gmail.com>
2025-11-28 00:25:38 +0700

    add: battlers attack

--- Assets/Resources/Entities/Battlers/Battlers.cs
+++ Assets/Resources/Entities/Battlers/Battlers.cs
@@ -6,16 +6,64 @@ public class Battlers : MonoBehaviour
     public bool isEnemy;
     public int price;
 
-    Rigidbody2D rb;
+    [SerializeField] Transform attackPoint;
+
+    private Rigidbody2D rb;
+    private float currentHealth;
+    private float nextAttackTime;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gameObject.layer = LayerMask.NameToLayer(isEnemy ? "Enemy" : "Player");
+        currentHealth = stat.GetHealth();
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocityX = stat.GetMoveSpeed() * (isEnemy ? 1 : -1);
+        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPoint.position, stat.GetAttackRange(), 0);
+
+        if (hitEnemies.Length > 0)
+        {
+            rb.linearVelocityX = 0;
+
+            if (Time.time > nextAttackTime)
+            {
+                foreach (Collider2D enemyCollider in hitEnemies)
+                {
+                    Battlers battler = enemyCollider.GetComponent<Battlers>();
+
+                    if (battler != null)
+                    {
+                        battler.TakeDamage(stat.GetDamage());
+                    }
+                }
+                nextAttackTime = Time.time + stat.GetAttackDelay();
+            }
+        }
+        else
+        {
+            rb.linearVelocityX = stat.GetMoveSpeed() * (isEnemy ? 1 : -1);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+        {
+            Destroy(gameObject);
+        }
+
+        print(currentHealth);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.DrawWireCube(attackPoint.position, new Vector3(stat.GetAttackRange().x, stat.GetAttackRange().y));
     }
 }
--- Assets/Resources/Entities/Battlers/BattlersStat.cs
+++ Assets/Resources/Entities/Battlers/BattlersStat.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 public class BattlersStat : ScriptableObject
 {
     [SerializeField] float health;
-    [SerializeField] float attack;
+    [SerializeField] float damage;
+    [SerializeField] Vector2 attackRange;
+    [SerializeField] float attackDelay;
     [SerializeField] float moveSpeed;
+
+    public float GetHealth()
+    {
+        return health;
+    }
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+
+    public float GetAttackDelay()
+    {
+        return attackDelay;
+    }
+
+    public Vector2 GetAttackRange()
+    {
+        return attackRange;
+    }
 
     public float GetMoveSpeed()
     {
