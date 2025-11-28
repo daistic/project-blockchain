8161ce8737bd90f5401bee4b2243bc5acf078b87
daistic <matotsuper@gmail.com>
2025-11-28 19:40:03 +0700

    add: drag camera clamping

--- Assets/Resources/Scenes/Levels/DragCamera.cs
+++ Assets/Resources/Scenes/Levels/DragCamera.cs
@@ -5,10 +5,12 @@ public class DragCamera : MonoBehaviour
 {
     public float dragSpeed = 2f;
 
-    private LevelInput levelInput;
+    [SerializeField] float leftBorder = -20f;
+    [SerializeField] float rightBorder = 20f;
 
+    private LevelInput levelInput;
     private bool isDragging = false;
-    private Vector3 dragOrigin;
+    private Vector2 lastMousePos;
 
     private void Awake()
     {
@@ -24,14 +26,14 @@ public class DragCamera : MonoBehaviour
 
     private void OnDisable()
     {
-        levelInput.Game.Click.started -= OnClickStarted;
+        levelInput.Game.Click.performed -= OnClickStarted;
         levelInput.Game.Click.canceled -= OnClickCanceled;
         levelInput.Game.Disable();
     }
 
     private void OnClickStarted(InputAction.CallbackContext ctx)
     {
-        dragOrigin = levelInput.Game.Point.ReadValue<Vector2>();
+        lastMousePos = levelInput.Game.Point.ReadValue<Vector2>();
         isDragging = true;
     }
 
@@ -44,11 +46,18 @@ public class DragCamera : MonoBehaviour
     {
         if (!isDragging) return;
 
-        Vector2 mousePos = levelInput.Game.Point.ReadValue<Vector2>();
-        Vector3 pos = Camera.main.ScreenToViewportPoint(mousePos - (Vector2)dragOrigin);
-        pos *= -1;
+        Vector2 currentMousePos = levelInput.Game.Point.ReadValue<Vector2>();
+        Vector3 delta = Camera.main.ScreenToViewportPoint(currentMousePos - lastMousePos);
+        Vector3 move = new Vector3(delta.x * dragSpeed, 0, delta.y * dragSpeed) * -1;
 
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-        transform.Translate(move, Space.World);
+        // Clamp the actual movement magnitude per frame to prevent huge jumps
+        move = Vector3.ClampMagnitude(move, 1.5f);
+
+        // Calculate proposed target position
+        Vector3 targetPosition = transform.position + move;
+        targetPosition.x = Mathf.Clamp(targetPosition.x, leftBorder, rightBorder);
+
+        transform.position = targetPosition;
+        lastMousePos = currentMousePos;
     }
 }
